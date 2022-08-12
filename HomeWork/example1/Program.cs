e26b31ebfad3304dedaa13ce3ff65cead6a6bc7d
kintai <warjlike@yandex.ru>
2022-08-12 19:18:11 +0300

    Homme_Example1

---
+++ HomeWork/example1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+int max = 0;
+if(number1 > number2)
+{
+    max = number1;
+}
+else
+{
+    max = number2;
+}
+Console.WriteLine($" a={number1},b={number2}->max={max}");
