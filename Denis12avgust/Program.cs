a0005aa14df4a5dacd89824a8467d8d5de8b65d1
kintai <warjlike@yandex.ru>
2022-08-12 21:35:05 +0300

    Denis12Augusta

---
+++ Denis12avgust/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine());
+int count = 1;
+string line = "";
+while(count < (number1 + 1))
+{
+line = ($"{line}{count * count},");
+count++;
+}
+Console.WriteLine($"{number1}->{line}");
