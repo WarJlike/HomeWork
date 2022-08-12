afa5253866cfb3dcbb7d593ea73b51a0b6eb3da9
kintai <warjlike@yandex.ru>
2022-08-12 20:23:45 +0300

    Homme_Example4

---
+++ HomeWork/example4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine());
+int count = 1;
+string line = "";
+while(count < (number1 + 1))
+{
+    if( count % 2 == 0)
+    {line = ($"{line}{count},");
+     count++;
+    }
+    else
+    {
+    count++;
+    }
+}
+Console.WriteLine($"{number1}->{line}");
