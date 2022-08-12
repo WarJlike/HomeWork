9d8a689459e18b7f6fb2fe60f645c8081e999e64
kintai <warjlike@yandex.ru>
2022-08-12 16:35:29 +0300

    Example_1

---
+++ задача1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+if(number1*number1 == number2)
+{
+    Console.WriteLine($"a={number2}; b={number1} -> да");
+}
+else
+{
+    Console.WriteLine($"a={number2}; b={number1} -> нет");
+}
