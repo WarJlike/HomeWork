472af0427354861e2109d00da388ca73cc6c1385
kintai <warjlike@yandex.ru>
2022-08-12 18:13:15 +0300

    Example_2

---
+++ задача 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите номер дня");
+int number = int.Parse(Console.ReadLine());
+string day = "такого дня нет";
+
+if(number == 1) day = "понедельник";
+if(number == 2) day = "вторник";
+if(number == 3) day = "среда";
+if(number == 4) day = "четверг";
+if(number == 5) day = "пятница";
+if(number == 6) day = "суббота";
+if(number == 7) day = "воскресенье";
+Console.WriteLine($"{number}->{day}");
