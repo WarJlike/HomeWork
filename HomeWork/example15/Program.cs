bae54c2b86942eea672176760c088d0a3a5f70cb
kintai <warjlike@yandex.ru>
2022-08-14 17:36:26 +0300

    Example15

---
+++ HomeWork/example15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число - ");
+int num = int.Parse(Console.ReadLine());
+if(num == 6 || num == 7)
+{
+    Console.WriteLine($"{num} -> да");
+}
+else
+{
+    Console.WriteLine($"{num} -> нет");
+}
