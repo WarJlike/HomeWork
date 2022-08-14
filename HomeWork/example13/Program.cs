218355d899e9f509c020560e82b79e3c4534f81a
kintai <warjlike@yandex.ru>
2022-08-14 17:32:15 +0300

    Example13

---
+++ HomeWork/example13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число - ");
+int num = int.Parse(Console.ReadLine());
+if(num > 99)
+{
+    Console.WriteLine($"{num} -> {(num/100) %10}");
+}
+else
+{
+    Console.WriteLine($"{num} -> третьей цифры нет");
+}
