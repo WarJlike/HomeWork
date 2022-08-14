41fd4d9aad657bdc20a1db7eb35a01a18fabe960
kintai <warjlike@yandex.ru>
2022-08-14 18:23:22 +0300

    Example19

---
+++ HomeWork/example19/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число - ");
+int num = int.Parse(Console.ReadLine());
+if(num/10000 == num % 10 && (num/1000) % 10 == (num / 10) % 10)
+{
+    Console.Write($"{num} -> да");
+}
+else
+{
+    Console.Write($"{num} -> нет");
+}
