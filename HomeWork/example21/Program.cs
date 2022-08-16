036926a1d08a170bbab695f9cab4896a08e03511
kintai <warjlike@yandex.ru>
2022-08-16 18:19:50 +0300

    Example21

---
+++ HomeWork/example21/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите x1");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y1");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите z1");
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите x2");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y2");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите z2");
+int z2 = int.Parse(Console.ReadLine());
+double dist;
+
+dist = Math.Sqrt(Math.Pow(x1 - x2,2) + Math.Pow(y1 - y2,2) + Math.Pow(z1 - z2,2));
+
+Console.WriteLine($"Расстояние между A({x1},{y1},{z1}) и В({x2},{y2},{z2}) равно {dist:f2} ");
+
+
