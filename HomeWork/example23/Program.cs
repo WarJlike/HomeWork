5622f188e8dabc37110368f0ed5ed6945a24bad9
kintai <warjlike@yandex.ru>
2022-08-16 17:56:12 +0300

    Example23

---
+++ HomeWork/example23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+string res = "";
+for (int i = 1; i <= num; i++)
+{
+    res = res + ($" {Math.Pow(i,3)},");
+}
+Console.WriteLine($"{num} ->{res}");
