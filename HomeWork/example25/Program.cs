4acb0e7e60bfa204c989e68481c12dc43e93f4a0
kintai <warjlike@yandex.ru>
2022-08-17 16:04:08 +0300

    Example25

---
+++ HomeWork/example25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите x1");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y1");
+int y1 = int.Parse(Console.ReadLine());
+
+void Method1(int num1, int num2)
+{
+int sum = 1;
+for (int i = 1; i <= num2; i++)
+{
+    sum = sum * num1;
+}
+Console.WriteLine($"{num1}, {num2} -> {sum}");
+}
+
+Method1(x1,y1);
