6d2958e9d9e0b3acdd7ceb053b28f59d7b9b2a2a
kintai <warjlike@yandex.ru>
2022-08-17 17:06:54 +0300

    Example27

---
+++ HomeWork/example27/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите x1");
+int x1 = int.Parse(Console.ReadLine());
+
+void Method1(int num)
+{
+int sum = 0;
+string str = num.ToString();
+for (int i = 0; i < str.Length; i++)
+{
+    sum = sum + str[i] -48;
+}
+Console.WriteLine($"{num} -> {sum}");
+}
+
+
+void Method2(int element)
+{
+ int element1 = element;
+ int sum = 0;
+ while(element1 > 0)
+ {
+    sum = sum + element1 % 10;
+    element1 = element1 / 10;
+ }
+Console.WriteLine($"{element} -> {sum}");
+}
+
+Method2(x1);
