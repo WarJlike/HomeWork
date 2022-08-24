b409f67523739970d90be3524d8fc69cf69ab652
kintai <warjlike@yandex.ru>
2022-08-24 17:02:57 +0300

    example43

---
+++ HomeWork/example43/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите k1 = ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b1 = ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k2 = ");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b2 = ");
+double b2 = double.Parse(Console.ReadLine());
+
+string Method1(double k1, double b1, double k2, double b2)
+{
+    double x = ( b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    string z = $"A({x};{y})";
+    return z;
+}
+Console.WriteLine($"функции y = {k1}x + {b1}, y = {k2}x +{b2} пересекаются в точке {Method1(k1, b1, k2, b2)}");
