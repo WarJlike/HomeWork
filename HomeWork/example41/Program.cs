ac9ae4ce0f398edcef59b7d31ffdd254fa31f0a6
kintai <warjlike@yandex.ru>
2022-08-24 17:16:11 +0300

    example41

---
+++ HomeWork/example41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите количество чисел -  ");
+int N = int.Parse(Console.ReadLine());
+int count = 0;
+
+for (int i = 1; i < N +1; i++)
+{
+    Console.WriteLine($"Введите {i} число -  ");
+    int x = int.Parse(Console.ReadLine());
+    
+    if(x > 0) count++;
+}
+Console.WriteLine($"пользователь ввел {count} чисел больше 0");
