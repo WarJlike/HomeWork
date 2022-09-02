c23d433f1bb849b7fdd982cd4a84b921c9ba4ac5
kintai <warjlike@yandex.ru>
2022-09-02 14:07:14 +0300

    koren

---
+++ HomeWork/koren/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+
+void SqrtArray(int a)
+{
+    for (int i = 0; i < a; i++)
+    {
+        if(i * i <= a && (i+1)*(i+1) > a)
+        {
+           Console.WriteLine($"{i} равен корню из {a}");
+        }
+    }
+}
+SqrtArray(n);
