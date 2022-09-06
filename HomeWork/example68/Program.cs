624bf0972f1cad62a0d39481896102debb31dca5
kintai <warjlike@yandex.ru>
2022-09-06 14:13:23 +0300

    example68

---
+++ HomeWork/example68/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число m");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n");
+int n = int.Parse(Console.ReadLine());
+
+int Method(int M, int N)
+{   
+   int akkerman = 0;
+   if(M == 0) akkerman = N +1;
+   else if(M > 0 && N == 0) akkerman =  Method(M - 1, 1);
+   else if(M > 0 && N > 0) akkerman = Method(M - 1, Method(M, N -1));
+return akkerman;
+}
+
+Console.WriteLine(Method(n, m));
